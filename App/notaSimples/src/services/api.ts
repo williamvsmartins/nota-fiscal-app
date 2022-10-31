b9b3df6ee13393ab3b219ab54bd97c4846a0a1c7
WilliamVSMartins <williamvaltherprogramador@gmail.com>
2022-10-31 20:48:13 -0300

    fix: solving api recognition error

--- App/notaSimples/src/services/api.ts
+++ App/notaSimples/src/services/api.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 import { API_NFC, QUERY_CNPJ } from '@env'
 
 export const apiNFC = axios.create({
-    baseURL: `${API_NFC}`
+    baseURL: API_NFC
 });
 
 export const queryCNPJ = axios.create({
-    baseURL: `${QUERY_CNPJ}`
+    baseURL:QUERY_CNPJ
 });
