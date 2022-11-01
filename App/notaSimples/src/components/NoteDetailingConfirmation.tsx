1c8ae526ec45ad7d5c4bd889915346b5c753d916
WilliamVSMartins <williamvaltherprogramador@gmail.com>
2022-11-01 15:51:30 -0300

    feat: create screen NoteDetailingConfirmation

---
+++ App/notaSimples/src/components/NoteDetailingConfirmation.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+} from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+
+
+import { ButtonConfirmation } from "./ButtonConfirmation";
+import colors from "../styles/colors";
+
+interface NoteDetailingConfirmationProps {
+    data: {
+        cnpj: string,
+        nome: string,
+        descricao: string,
+        quantidade: string,
+        valor: string
+    }
+}
+
+export function NoteDetailingConfirmation({data, ...rest} : NoteDetailingConfirmationProps) {
+
+    const navigator = useNavigation();
+    function handleClient() {
+        navigator.navigate('NoteDetailing', {
+            cnpj: data.cnpj,
+        })
+    }
+    return (
+        <View style={styles.container}>
+           <View style={styles.content}>
+                <View style={styles.identification}>
+                    <Text> {data.cnpj} </Text>
+                    <Text> {data.nome} </Text>
+                    <Text> {data.descricao} </Text>
+                    <Text> {data.quantidade} </Text>
+                </View>
+           </View>
+
+            <ButtonConfirmation 
+                title="Confirmar"
+            />
+                    
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    content: {
+
+    },
+    identification: {
+
+    }
+})
