703d624d8a6d6386a2543615b0be3b8423076fe8
WilliamVSMartins <williamvaltherprogramador@gmail.com>
2022-11-01 15:52:29 -0300

    build: add expo-updates

--- App/notaSimples/App.tsx
+++ App/notaSimples/App.tsx
@@ -1,9 +1,28 @@
+import React, { useEffect } from 'react';
 import { View } from "react-native";
+import * as Updates from 'expo-updates';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { AuthProvider } from './src/contexts/AuthContext';
 import { Routes } from './src/routes';
 
 export default function App() {
+
+  useEffect(() => {
+    async function updateApp() {
+      const { isAvailable } = await Updates.checkForUpdateAsync();
+      if (isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync(); // depende da sua estratégia
+      }
+    }
+    updateApp();
+  }, []);
+
+  useEffect(() => {
+   AsyncStorage.clear()
+  }, [])
   return (
     <View style={{ flex: 1}}>
       <AuthProvider>
